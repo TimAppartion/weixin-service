78af84bf00badf0226877402bf2e71ba4de031e3
TimAppartion <wh2234040>
2022-09-30 11:49:36 +0800

    日志打印修改

--- src/main/java/com/example/weixin/user/controller/CouponsController.java
+++ src/main/java/com/example/weixin/user/controller/CouponsController.java
@@ -4,6 +4,7 @@ package com.example.weixin.user.controller;
 import com.example.weixin.user.mapper.CouponsDetailsMapper;
 import com.example.weixin.user.model.CouponsDetails;
 import com.example.weixin.user.service.CouponsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+
+@Slf4j
 
 @RestController
 @RequestMapping("/controller/Coupons")
@@ -25,6 +28,7 @@ public class CouponsController {
 
     @RequestMapping("/getList")
     public List<CouponsDetails> getList(){
+        log.info("我直接一个哒唛");
         return couponsService.getList();
     }
 
